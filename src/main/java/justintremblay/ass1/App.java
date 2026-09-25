ac76c585f915cee170fdc4073c1c4f610e71b6d6
justinTremblayyy <2582154@edu.vaniercollege.qc.ca>
2026-09-25 18:18:11 -0400

    added the sample texts and the label to show them

--- src/main/java/justintremblay/ass1/App.java
+++ src/main/java/justintremblay/ass1/App.java
@@ -11,9 +11,28 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application {
+    
+    
+        private final String[] texts = {
+        "Try typing this text. Do it as quickly and accurately as you can.",
+        "Next type another line of input data.",
+        "The quick brown fox jumps over the lazy dog.",
+        "Five big quacking zephyrs jolt my wax bed.",
+        "Sympathizing would fix Quaker objectives.",
+        "A large fawn jumped quickly over white zinc boxes."
+    };
+
+    private int textNum = 0;
+
+    private Label lblText;
 
     @Override
     public void start(Stage primaryStage) {
+        
+        lblText = new Label();
+        lblText.setWrapText(true);
+        lblText.setStyle("-fx-font-size: 18px;");
+        
         primaryStage.setTitle("Typing Tutor");
         primaryStage.setScene(scene);
         primaryStage.show();
