ac7a8509fbae8854da6db2062117910c1c098a55
justinTremblayyy <2582154@edu.vaniercollege.qc.ca>
2026-09-25 17:07:47 -0400

    created the file

---
+++ src/main/java/justintremblay/ass1/SystemInfo.java
@@ -0,0 +1,13 @@
+package justintremblay.ass1;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module justintremblay.ass1 {
+    requires javafx.controls;
+    exports justintremblay.ass1;
+}
